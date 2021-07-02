fac336c8eb39ab31748eadb726b781aed46ff760
mireie <michael.reiersgaard@gmail.com>
2021-07-02 09:06:21 -0700

    add initial exchange JS, HTML

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import exchangeService from './js/exchangeService';
+import {ExchangeService} from './js/exchangeService';
+
+$(document).ready(()=> {
+  $()
+})
 
 
 
